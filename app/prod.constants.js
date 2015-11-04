b09281be000bcb846959aceb1dc8dac1c213b5dd
Birju Shah <birju@Birjus-MacBook-Pro.local>
2015-11-05 02:53:21 +0530

    changing to https

--- app/prod.constants.js
+++ app/prod.constants.js
@@ -1,6 +1,6 @@
 var config = {
-	apiUrl:'http://52.25.132.250:8080',
-	frontUrl:'http://forums.qiku.com',
+	apiUrl:'https://inforum.qiku.com',
+	frontUrl:'https://forums.qiku.com',
 	facebook:{
 		appId:'906239946095792'
 	}
