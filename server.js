26d38ee0a98b9a3bc9d2035f00b47d6f19557d19
Birju Shah <birju@Birjus-MacBook-Pro.local>
2015-11-18 17:10:13 +0530

    robots txt

--- server.js
+++ server.js
@@ -13,13 +13,19 @@ app.set('view engine', 'handlebars');
 app.set('views','/');
 app.engine('.html', exphbs({extname: '.html',layoutsDir:'/'}));
 app.use('/app', express.static (__dirname + '/app/'));
+//app.get('/robots.txt',function(req,res){res.render(__dirname+'/robots.txt',{layout:false})});
 app.get('/*', function (req, res) {
-  res.render(__dirname+'/index-'+process.env.NODE_ENV+'.html',{
-  	layout:false,
-  	fbAppId:fbAppId,
-  	frontUrl:frontUrl,
-  	env:process.env.NODE_ENV
-  });
+  if('/robots.txt' === req.url){
+    res.type('text/plain');
+    res.send('User-agent: *\nDisallow:');
+  }else{
+    res.render(__dirname+'/index-'+process.env.NODE_ENV+'.html',{
+      layout:false,
+      fbAppId:fbAppId,
+      frontUrl:frontUrl,
+      env:process.env.NODE_ENV
+    });
+  }
 });
  
 app.listen(3000);
